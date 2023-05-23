e3e3bb96f0fcb7864755f4536377f7909de21467
Abel Cierto <u20201b561@upc.edu.pe>
2023-05-23 12:15:33 -0500

    feat: added model and service
    Added the model and service of input information and payment plan

---
+++ src/app/inputdata/model/input-data.ts
@@ -0,0 +1,13 @@
+export interface InputData {
+    annualEffectiveCostRate: number,
+    annualPropertyInsurance: number,
+    currency: string,
+    customerFirstName: string,
+    housingPrice: number,
+    id: number,
+    initialFee: number,
+    monthlyDeductionInsurance: number,
+    receivedHousingSupport: boolean,
+    sustainableHousing: boolean,
+    term: number,
+}
